cdef75b40eaa6b2e9862b2ad78fa86e23503744d
Amirdhesh <amirdhesh2003@gmail.com>
2024-04-26 10:44:32 +0530

    JWT token functions are created�

---
+++ backend/app/core/security.py
@@ -0,0 +1,23 @@
+from datetime import timedelta,datetime
+import jwt
+import bcrypt
+from pytz import timezone
+from uuid import UUID
+from app.core.settings import settings
+
+
+
+async def create_token(id : UUID , email : str):
+    ist = timezone("Asia/kolkata")
+    expiry = datetime.now(ist) + timedelta(hours= 18)
+    payload= {"id" : str(id),"email":email,"exp" : expiry}
+    token = jwt.encode(payload,settings.JWT_SECRET_KEY,settings.JWT_ALGORITHM)
+    return token
+
+
+async def decode_token(token : str):
+    try : 
+        payload = jwt.decode(token,settings.JWT_SECRET_KEY,settings.JWT_ALGORITHM)
+        return payload
+    except:
+        return None
