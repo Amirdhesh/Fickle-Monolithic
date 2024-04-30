a782c12073ffe75dcbabc31f84c18a6af06b50e4
Amirdhesh <amirdhesh2003@gmail.com>
2024-04-30 14:55:01 +0530

    ✏ Updated migration

--- backend/app/migration/env.py
+++ backend/app/migration/env.py
@@ -7,8 +7,9 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+
 from app.core.settings import settings
-from app.model import * #noqa: F403
+from app.model import *  # noqa: F403
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +19,7 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-config.set_main_option('sqlalchemy.url',settings.DB_URI)
+config.set_main_option("sqlalchemy.url", settings.DB_URI)
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
