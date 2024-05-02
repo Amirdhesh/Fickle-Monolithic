66add85ecbf0b5f7cbc9798baac5658031528d60
Amirdhesh <amirdhesh2003@gmail.com>
2024-05-02 21:19:58 +0530

    new endpoints are added

--- backend/app/api/api_v1/endpoints/login.py
+++ backend/app/api/api_v1/endpoints/login.py
@@ -1,11 +1,29 @@
-from fastapi import APIRouter,responses,Cookie
-from app.schema.user import userCreate
-from app.core.db_init import async_session
-from sqlalchemy.ext.asyncio.session import AsyncSession
+from fastapi import APIRouter, Response
+from app.core.db_init import async_session,redis_connection
+from app.core.settings import settings
+from app.core.security import user_credentials
+from app.schema.user import loginUser,message
+from app.crud.user import login_user
+
 route = APIRouter()
 
 
+@route.post("/login",status_code=200,response_model=message)
+async def login_users(*,session:async_session,login_detail:loginUser,response: Response):
+    
+    token =await login_user(session=session,user_login=login_detail)
+    response.set_cookie(key = "fickel_token",value=token,expires=(settings.JWT_EXPIRY_TIME*3600))
+    return message(
+        message=f"Login successfull👍",   
+    )
 
-@route.post("/register")
-async def create_user(session: async_session):
-    ...
+
+@route.delete("/logout",status_code=204)
+async def logout_user(*,response:Response,token:str):
+    response.delete_cookie("fickel_token")
+    data = await user_credentials(token)
+    await redis_connection.delete(f"access_token_{data.id}")
+    print("Hello")
+    return message(
+        message="Logout successfull👍."
+    )
--- backend/app/api/api_v1/endpoints/user.py
+++ backend/app/api/api_v1/endpoints/user.py
@@ -1,17 +1,34 @@
-from fastapi import APIRouter, Response, Cookie
-from app.schema.user import userCreate
+from typing import Annotated,Union
+from fastapi import Cookie,APIRouter, Response, HTTPException
+from app.schema.user import userCreate,message
 from app.core.db_init import async_session
-from app.schema.user import userCreate,userRead
-from app.crud.user import add_user
-from app.core.security import create_token
-
+from app.schema.user import userCreate,userRead,TokenResponse,changePassword,userUpdate
+from app.crud.user import add_user,update_user,change_password
+from app.core.security import create_token,user_credentials
+from app.core.settings import settings
 
 route = APIRouter()
 
 
-@route.post("/register")
+@route.post("/register",status_code=201,response_model=message)
 async def create_user(session: async_session, user_in: userCreate,response: Response): 
     user: userRead = await add_user(session=session,user_create=user_in)
-    token = create_token(id=user.id , email=user.id)
-    response.set_cookie(key = "fickel_token",value=token)
-    return {"User successfully added."}
+    token = await create_token(id=user.id , email=user.id)
+    response.set_cookie(key = "fickel_token",value=token,expires=(settings.JWT_EXPIRY_TIME*3600))
+    return message(
+        message="User has been successfully added."
+        )
+
+
+@route.patch("/change_password")
+async def Password_change(session:async_session,password:changePassword,token:str):
+    data =await user_credentials(token=token)
+    if password.new_password != password.reenter_password:
+        raise HTTPException(
+            status_code=400,
+            detail="Both are different passwords."
+        )
+    await change_password(session=session,password=password,id = data.id)
+    return message(
+        message="User updated successfully."
+    )
