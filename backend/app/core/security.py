f4f43c1878d5d119d069f45c894308ac7d2119c1
Amirdhesh <amirdhesh2003@gmail.com>
2024-05-02 21:19:04 +0530

    Error solved

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,23 +1,53 @@
-from datetime import timedelta,datetime
 import jwt
 import bcrypt
+from fastapi import HTTPException
+from datetime import timedelta, datetime
 from pytz import timezone
 from uuid import UUID
 from app.core.settings import settings
+from app.core.db_init import redis_connection
+from app.schema.user import TokenResponse
+from uuid import UUID
+
+def hash_password(password: str):
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode('utf-8')
 
 
+def check_pw(password: str, hashed_password: str):
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode('utf-8'))
 
-async def create_token(id : UUID , email : str):
+
+async def create_token(id: UUID, email: str):
     ist = timezone("Asia/kolkata")
-    expiry = datetime.now(ist) + timedelta(hours= settings.JWT_EXPIRY_TIME)
-    payload= {"id" : str(id),"email":email,"exp" : expiry}
-    token = jwt.encode(payload,settings.JWT_SECRET_KEY,settings.JWT_ALGORITHM)
+    expiry = datetime.now(ist) + timedelta(hours=settings.JWT_EXPIRY_TIME)
+    payload = {"id": str(id), "email": email, "exp": expiry}
+    token = jwt.encode(payload, settings.JWT_SECRET_KEY, settings.JWT_ALGORITHM)
+    await redis_connection.setex(
+        name = f'access_token_{id}',
+        value = token,
+        time = settings.JWT_EXPIRY_TIME * 3600
+    )
     return token
 
 
-async def decode_token(token : str):
-    try : 
-        payload = jwt.decode(token,settings.JWT_SECRET_KEY,settings.JWT_ALGORITHM)
+async def decode_token(token: str):
+    try:
+        payload = jwt.decode(token, settings.JWT_SECRET_KEY, settings.JWT_ALGORITHM)
         return payload
     except:
         return None
+
+
+async def user_credentials(token: str):
+    try :
+        data =await decode_token(token=token)
+        if not data:
+            raise ValueError("Invalid Token 1")
+        redis_token = await redis_connection.get(f"access_token_{data['id']}")
+        if redis_token:
+            if redis_token == token:
+                return TokenResponse(id = UUID(data["id"]),email=data["email"])
+            raise ValueError("Invalid Token 2")
+        raise ValueError("Invalid Token 3")
+    except Exception as e:
+        raise HTTPException(status_code=401,detail=f'{e}')
