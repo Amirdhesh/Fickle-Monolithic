5292c93d4658d3522fcb70006c360132eef1f2d7
Amirdhesh <amirdhesh2003@gmail.com>
2024-05-02 21:16:12 +0530

    updated schema

--- backend/app/schema/user.py
+++ backend/app/schema/user.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 from app.schema.baseuuid import baseUUID
-
+from uuid import UUID
 
 class userBase(SQLModel):
     Name: str = Field(index=True)
@@ -22,10 +22,28 @@ class userRead(baseUUID, userBase):
 class userUpdate(SQLModel):
     Name: Optional[str]
     email: Optional[str]
-    password: str
+    password: Optional[str]
     about: Optional[str]
 
 
 class userLogin(SQLModel):
     email: str
     password: str
+
+
+class message(SQLModel):
+    message: str
+
+
+class changePassword(SQLModel):
+    new_password:str
+    reenter_password:str
+
+
+class TokenResponse(SQLModel):
+    id:UUID
+    email:str
+
+class loginUser(SQLModel):
+    email:str
+    password:str
