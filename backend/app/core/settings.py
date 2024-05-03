a11ef1aa5e0f779fe5769edf27fe7f0d9dba4c52
Amirdhesh <amirdhesh2003@gmail.com>
2024-05-03 12:31:31 +0530

    celery backend update

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -15,12 +15,14 @@ class Settings(BaseSettings):
     REDIS_HOST: str = os.environ.get("redis_host")
     REDIS_PORT: str = os.environ.get("redis_port")
     REDIS_DB: int = os.environ.get("redis_database")
-    CELERY_BROKER: str = os.environ.get("celery_backend")
+    CELERY_BROKER: str = os.environ.get("celery_broker")
+    CELERY_BACKEND: str = os.environ.get("celery_backend")
     LOGGING_INI: str = "logging.ini"
     LOG_FILE_NAME: str = "fickle.log"
-    LOG_FILE_SIZE: str = os.environ.get('log_file_size')
-    LOG_FILE_BACKUP: str = os.environ.get('log_file_backup')
-    LOGGING_PLATFORM_TOKEN: str = os.environ.get('betterstack_token')
+    LOG_FILE_SIZE: str = os.environ.get("log_file_size")
+    LOG_FILE_BACKUP: str = os.environ.get("log_file_backup")
+    LOGGING_PLATFORM_TOKEN: str = os.environ.get("betterstack_token")
+
 
 
 settings = Settings()
