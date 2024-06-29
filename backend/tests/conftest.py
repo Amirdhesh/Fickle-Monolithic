d6ab03e78b8123dd56956fc5cc88c8c100f3d6a7
Amirdhesh <amirdhesh2003@gmail.com>
2024-06-29 14:56:34 +0530

    ✅ Started to work with docker.

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,4 +1,3 @@
-import pytest
 import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
 from main import app
@@ -9,6 +8,7 @@ from sqlalchemy.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, async_sessionmaker
 
 async_engine: AsyncEngine = create_async_engine(settings.TEST_DB_URI)
+
 
 @pytest_asyncio.fixture(scope="session")
 async def override_get_async_session():
@@ -22,11 +22,14 @@ async def override_get_async_session():
     async with async_engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.drop_all)
     await async_engine.dispose()
-        
+
 
 app.dependency_overrides[get_async_session] = override_get_async_session
 
+
 @pytest_asyncio.fixture
 async def async_client():
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as client:
         yield client
--- backend/tests/test_endpoints/test_user.py
+++ backend/tests/test_endpoints/test_user.py
@@ -1,6 +1,6 @@
 import pytest
-import pytest_asyncio
 from httpx import AsyncClient
+
 
 @pytest.mark.asyncio
 async def test_health(async_client: AsyncClient):
